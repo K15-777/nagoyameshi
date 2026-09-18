ec37c93c1fc1957cded2ccd7fe97c1b8ef4fa4a1
4mg <4mg@4MG-D-KOIZUMI>
2026-09-18 19:58:40 +0900

    9/18-2コミット

---
+++ src/main/java/com/example/nagoyameshi/form/UserEditForm.java
@@ -0,0 +1,25 @@
+package com.example.nagoyameshi.form;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserEditForm {
+	@NotNull
+	private Integer id;
+	
+	@NotBlank(message = "氏名を入力してください。")
+	private String name;
+	
+	@NotBlank(message = "フリガナを入力してください。")
+	private String furigana;
+	
+	@NotBlank(message = "メールアドレスを入力してください。")
+	@Email(message = "メールアドレスは正しい形式で入力してください。")
+	private String email;
+}
